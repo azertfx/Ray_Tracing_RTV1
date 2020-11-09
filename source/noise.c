4c7dd319d9f6ce1a8654bc688b76d6ed94bba3c0
hamza-ezzahiry <hamza.ezzahiry@gmail.com>
2020-11-09 01:06:44 +0100

    make perlin noise works + fix norme

--- source/noise.c
+++ source/noise.c
@@ -6,22 +6,13 @@
 /*   By: hezzahir <hamza.ezzahiry@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 11:05:07 by hezzahir          #+#    #+#             */
-/*   Updated: 2020/11/08 22:21:02 by hezzahir         ###   ########.fr       */
+/*   Updated: 2020/11/09 01:06:07 by hezzahir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-/*
-**	Disruption id :
-**				*) id = 1 : Checkboard
-**				*) id = 2 : XOR
-**				*) id = 3 : Perlin
-**				*) id = 4 : Perlin sexy 🤩😍
-**				*) id = 4 :
-*/
-
-void	generate_noise(void)
+void		generate_noise(void)
 {
 	int	x;
 	int	y;
@@ -33,13 +24,17 @@ void	generate_noise(void)
 	while (z++ < noiseDepth)
 		while (y++ < noiseHeight)
 			while (x++ < noiseWidth)
-				noise[z][y][x] = (rand() % 32768) / 32768.0;
+				g_noise[z][y][x] = (rand() % 32768) / 32768.0;
+	x = 0;
+	while (x < 256)
+	{
+		g_p[x] = rand() % 256;
+		g_p[256 + x] = g_p[x];
+		x++;
+	}
 }
 
-/*
-** Noice checkboard / Damier
-*/
-void	checkboard(t_vect point, t_vect *color, double x)
+void		checkboard(t_vect point, t_vect *color, double x)
 {
 	float	sines;
 
@@ -50,43 +45,36 @@ void	checkboard(t_vect point, t_vect *color, double x)
 		*color = (t_vect){31.0, 151.0, 206.0};
 }
 
-/*
-** Noice XOR
-*/
-
 t_vect		noise_xor(t_vect color, t_vect v)
 {
 	uint8_t	c;
 
-	c = ((int) v.x ^ (int) v.y ^ (int) v.z);
+	c = ((int)v.x ^ (int)v.y ^ (int)v.z);
 	color.x = 255 - c;
 	color.y = c;
 	color.z = c % 128;
 	return (color);
 }
 
-// t_vect		perlin(t_vect color, t_vect v)
-// {
-// 	double	n;
+void		perlin(t_vect v, t_vect *color)
+{
+	double	n;
 
-// 	n = pnoise(v.x, v.y, v.z);
-// 	n = n - floor(n);
-// 	color.x = floor(255 * n);
-// 	color.y = floor(255 * n);
-// 	color.z = floor(255 * n);
-// 	return (color);
-// }
-
+	n = pnoise(v.x, v.y, v.z);
+	n = n - floor(n);
+	color->x = floor(255 * n);
+	color->y = floor(255 * n);
+	color->z = floor(255 * n);
+}
 
 void		apply_noise(t_point *p)
 {
-  	//printf("|%d|\n", p->obj->dsp);
 	if (p->obj->dsp == CHECKBOARD)
 		checkboard(p->p_inter, &p->p_color, 0.3);
 	else if (p->obj->dsp == XOR)
 		p->p_color = noise_xor(p->p_color, p->p_inter);
 	else if (p->obj->dsp == WOOD)
 		wood(p->p_inter, &p->p_color, 10);
-	/*else if (p->obj->dsp == PERLIN)
-		perlin(p->p_inter, &p->p_color);*/
+	else if (p->obj->dsp == PERLIN)
+		perlin(p->p_inter, &p->p_color);
 }
