708671e79331b6fd428d7ff16b81358c6e661c65
hamza-ezzahiry <hamza.ezzahiry@gmail.com>
2020-11-09 04:21:37 +0100

    add function for up + modify perlin

--- source/slice_xyz.c
+++ source/slice_xyz.c
@@ -6,7 +6,7 @@
 /*   By: hezzahir <hamza.ezzahiry@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 19:06:26 by hezzahir          #+#    #+#             */
-/*   Updated: 2020/11/04 05:32:24 by hezzahir         ###   ########.fr       */
+/*   Updated: 2020/11/09 03:29:17 by hezzahir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,21 +43,34 @@ double	check_up(t_vect pt_inter, t_vect lim, double t)
 	return (t);
 }
 
+double	check_down(t_vect pt_inter, t_vect lim, double t)
+{
+	if (is_included(pt_inter.x, lim.x))
+		return (t);
+	else if (is_included(pt_inter.y, lim.y))
+		return (t);
+	else if (is_included(pt_inter.z, lim.z))
+		return (t);
+	else if (is_included2(pt_inter.x, lim.x))
+		return (t);
+	else if (is_included2(pt_inter.y, lim.y))
+		return (t);
+	else if (is_included2(pt_inter.z, lim.z))
+		return (t);
+	return (0);
+}
+
 double	ft_slice(t_ray r, t_obj *obj, double t)
 {
-	t_vect pt_inter;
+	t_vect	pt_inter;
 
 	pt_inter = ft_vect_add(r.ori, ft_vect_mult_nbr(r.dir, t));
 	if (obj->slc)
 	{
 		if (obj->uod)
-		{
 			return (check_up(pt_inter, obj->lim, t));
-		}
 		else
-		{
-			printf("here");
-		}
+			return (check_down(pt_inter, obj->lim, t));
 	}
 	return (t);
 }
