5ab0d1576d0f75186609764b7e54b4f948955745
hamza-ezzahiry <hamza.ezzahiry@gmail.com>
2020-11-11 17:41:57 +0100

    fix norme

--- include/struct_rt.h
+++ include/struct_rt.h
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   struct_rt.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hastid <hastid@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hezzahir <hamza.ezzahiry@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 06:38:06 by hastid            #+#    #+#             */
-/*   Updated: 2020/11/09 03:05:34 by hastid           ###   ########.fr       */
+/*   Updated: 2020/11/11 17:27:30 by hezzahir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_RT_H
 #define STRUCT_RT_H
 
-typedef struct s_vect
+typedef struct	s_vect
 {
-	double x;
-	double y;
-	double z;
-} t_vect;
+	double		x;
+	double		y;
+	double		z;
+}				t_vect;
 
 typedef struct	s_cordin
 {
@@ -65,32 +65,32 @@ typedef struct	s_inter
 typedef struct	s_texture
 {
 
-	int		t;
-	int		*buf;
-	void	*img;
-	double	Um;
-	double	Vm;
-	int		width;
-	int		height;
-} t_texture;
+	int			t;
+	int			*buf;
+	void		*img;
+	double		Um;
+	double		Vm;
+	int			width;
+	int			height;
+}				t_texture;
 
-typedef struct s_mlx
+typedef struct	s_mlx
 {
-	int bpp;
-	int size_l;
-	int endian;
-	void *mlx_ptr;
-	void *win_ptr;
-	void *img_ptr;
-	unsigned char *img_data;
-} t_mlx;
+	int				bpp;
+	int				size_l;
+	int				endian;
+	void			*mlx_ptr;
+	void			*win_ptr;
+	void			*img_ptr;
+	unsigned char	*img_data;
+}				t_mlx;
 
-typedef struct s_ray
+typedef struct	s_ray
 {
-	t_vect ori;
-	t_vect dir;
-	double time;
-} t_ray;
+	t_vect		ori;
+	t_vect		dir;
+	double		time;
+}				t_ray;
 
 typedef struct	s_sc
 {
@@ -102,108 +102,109 @@ typedef struct	s_sc
 	double	amb;
 }				t_sc;
 
-typedef struct s_cam
+typedef struct	s_cam
 {
-	int opt;
-	t_vect x;
-	t_vect y;
-	t_vect z;
-	double i;
-	double j;
-	double fov;
-	double width;
-	double height;
-	t_vect ori;
-	t_vect tar;
-} t_cam;
+	int			opt;
+	t_vect		x;
+	t_vect		y;
+	t_vect		z;
+	double		i;
+	double		j;
+	double		fov;
+	double		width;
+	double		height;
+	t_vect		ori;
+	t_vect		tar;
+}				t_cam;
 
-typedef struct s_obj
+typedef struct		s_obj
 {
-	int id;
-	int opt;
-	int neg;
-	int trs;
-	int rfl;
-	int slc;
-	int uod;
-	int dsp;
-	t_vect	e1;
-	t_vect	e2;
-	double ray;
-	t_vect ori;
-	t_vect col;
-	t_vect axi;
-	t_vect tra;
-	t_vect rot;
-	t_vect lim;
-	double width;
-	double height;
-	double nbr_t;
-	double t_max;
-	t_texture 	txt;
-	struct s_obj *next;
-} t_obj;
+	int				id;
+	int				opt;
+	int				neg;
+	int				trs;
+	int				rfl;
+	int				slc;
+	int				uod;
+	int				dsp;
+	int				WhatIfound;
+	t_vect			e1;
+	t_vect			e2;
+	double			ray;
+	t_vect			ori;
+	t_vect			col;
+	t_vect			axi;
+	t_vect			tra;
+	t_vect			rot;
+	t_vect			lim;
+	double			width;
+	double			height;
+	double			nbr_t;
+	double			t_max;
+	t_texture		txt;
+	struct s_obj	*next;
+}					t_obj;
 
-typedef struct s_light
+typedef struct		s_light
 {
-	int id;
-	int opt;
-	double pow;
-	t_vect ori;
-	t_vect col;
-	t_vect axi;
-	double ang;
-	struct s_light *next;
-} t_light;
+	int				id;
+	int				opt;
+	double			pow;
+	t_vect			ori;
+	t_vect			col;
+	t_vect			axi;
+	double			ang;
+	struct s_light	*next;
+}					t_light;
 
-typedef struct s_plight
+typedef struct	s_plight
 {
-	t_vect amb;
-	t_vect def;
-	t_vect spc;
-	double light;
-} t_plight;
+	t_vect		amb;
+	t_vect		def;
+	t_vect		spc;
+	double		light;
+}				t_plight;
 
-typedef struct s_point
+typedef struct	s_point
 {
-	t_obj *obj;
-	t_vect p_dir;
-	t_vect p_inter;
-	t_vect p_normal;
-	t_vect p_color;
-	t_plight p_light;
-	double inter_min;
-} t_point;
+	t_obj		*obj;
+	t_vect		p_dir;
+	t_vect		p_inter;
+	t_vect		p_normal;
+	t_vect		p_color;
+	t_plight	p_light;
+	double		inter_min;
+}				t_point;
 
-typedef struct s_event
+typedef struct	s_event
 {
-	char *file;
-	char *file_origin;
-	int	move_z;
-	int	move_x;
-	int	rot_y;
-	int	rot_x;
-} t_event;
+	char		*file;
+	char		*file_origin;
+	int			move_z;
+	int			move_x;
+	int			rot_y;
+	int			rot_x;
+}				t_event;
 
-typedef struct s_thread
+typedef struct	s_thread
 {
-	int start;
-	int end;
-	t_ray ray;
-	t_vect color;
-} t_thread;
+	int			start;
+	int			end;
+	t_ray		ray;
+	t_vect		color;
+}				t_thread;
 
 typedef struct	s_rt
 {
-	int id;
-	t_sc	s;
-	t_mlx	m;
-	t_cam	*c;
-	t_obj	*o;
-	t_light	*l;
-	t_point point;
-	t_event event;
-	t_thread thread;
-}	t_rt;
+	int			id;
+	t_sc		s;
+	t_mlx		m;
+	t_cam		*c;
+	t_obj		*o;
+	t_light		*l;
+	t_point		point;
+	t_event		event;
+	t_thread	thread;
+}				t_rt;
 
 #endif
